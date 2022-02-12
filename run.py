1062cb86fe38e31b5d3db05e77f8bd1bf8a35d78
Divine-97 <kumwendadivine@gmail.com>
2022-02-12 21:04:10 +0000

    Initial commit

---
+++ run.py
@@ -0,0 +1,10 @@
+def start():
+    """
+    Run all program functions
+    """
+    print("Welcome to Quizzer")
+    username = input("Username?: ")
+    print("hey",username,"\nLets get started for every good answer you get one point!")
+
+
+
